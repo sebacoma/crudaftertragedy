e772df541fc2f5525b458c12dc2b8585825d9e53
sebacoma <114264296+sebacoma@users.noreply.github.com>
2023-11-23 23:57:00 -0300

    first commit despues de la tragedia

---
+++ back/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// routes for users
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// route for points
+Route::put('/users/{user_id}/update-points/{points}/{operation_type}', [UserController::class, 'update_points']);
